79795d2beb2da0c211eb56e528bb117267dff514
George Semenov <george.sem@live.ru>
2021-07-08 23:39:58 +0300

    Update UPDATE.bat

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,6 +1,7 @@
-git remote add template https://github.com/academy-project-programming/all-tasks > nul 2>&1
+git remote add template https://github.com/academy-project-programming/all-tasks
 git stash
 git pull origin
+git fetch template
 git merge -s recursive -X ours --allow-unrelated-histories template/master
 git checkout template/master -- README.md
 git checkout template/master -- UPDATE.bat
