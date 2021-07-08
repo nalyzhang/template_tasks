b0cc65eb3c732fec2a3b827e77cf9a104a65bcde
George Semenov <george.sem@live.ru>
2021-07-08 20:49:19 +0300

    Update UPDATE.bat

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,5 +1,5 @@
 git stash
-git fetch --all
-git pull template master -s recursive -X theirs --rebase --allow-unrelated-histories
+git pull -s recursive -X theirs --rebase --allow-unrelated-histories template master
+git pull -s recursive -X theirs --rebase --allow-unrelated-histories origin master
 git push origin
 git stash pop
