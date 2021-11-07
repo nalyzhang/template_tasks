c804c86783e3ee5d98dc7802b5131104000e7d38
nalyzhang <90562582+nalyzhang@users.noreply.github.com>
2021-11-07 19:13:11 +0300

    Update I_Enumerations.java

--- L2_Datatypes/I_Enumerations.java
+++ L2_Datatypes/I_Enumerations.java
@@ -2,6 +2,32 @@ package L2_Datatypes;
 
 public class I_Enumerations {
     public static void main(String[] args) {
-
+        enum SchoolSubjects {
+            Math,
+            Russian,
+            Informatics,
+            Physics,
+            English,
+            German,
+            Astronomy,
+            Chemistry,
+            Literature,
+            History
+        }
+        SchoolSubjects lesson1 = SchoolSubjects.History;
+        SchoolSubjects lesson2 = SchoolSubjects.Literature;
+        SchoolSubjects lesson3 = SchoolSubjects.Literature;
+        SchoolSubjects lesson4 = SchoolSubjects.Math;
+        SchoolSubjects lesson5 = SchoolSubjects.Math;
+        SchoolSubjects lesson6 = SchoolSubjects.Physics;
+        SchoolSubjects lesson7 = SchoolSubjects.English;
+        System.out.println("Расписание уроков");
+        System.out.println("1. " + lesson1);
+        System.out.println("2. " + lesson2);
+        System.out.println("3. " + lesson3);
+        System.out.println("4. " + lesson4);
+        System.out.println("5. " + lesson5);
+        System.out.println("6. " + lesson6);
+        System.out.println("7. " + lesson7);
     }
 }
